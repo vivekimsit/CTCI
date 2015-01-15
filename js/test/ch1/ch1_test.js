af0b40f7cfa5d252805c397f9e61b54c2f315f57
Vivek Poddar <vivekimsit@gmail.com>
2015-01-15 15:44:55 +0530

    Adds failing tests.

--- js/test/ch1/ch1_test.js
+++ js/test/ch1/ch1_test.js
@@ -3,5 +3,7 @@
 require('../test_helper.js');
 
 describe('unique_chars', function() {
-  it('should return something');
+  it('returns false if chars are different');
+  it('returns true if chars are same');
+  it('returns true if string is empty');
 });
